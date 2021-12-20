e6cedaf1f064c088a53f37cdeea6cc14c7c45677
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2021-12-21 00:51:46 +0900

    Delete redundant files

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -19,15 +19,11 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-# Set the project base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
