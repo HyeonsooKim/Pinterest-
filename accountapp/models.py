2ef66cc3442d6fee03dc391166fd23646a65f7a6
HyeonsooKim <48047773+HyeonsooKim@users.noreply.github.com>
2021-12-21 16:41:01 +0900

    django course 14 commit

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class HelloWorld(models.Model):
+    #text 한 줄, charField는 문자열, null은 없어도 되는 정보인지 물어보는 것
+    text = models.CharField(max_length=255, null=False)
